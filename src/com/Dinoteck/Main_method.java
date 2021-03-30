e7a7e31c366f5ac644870009ada24988fde43c37
L00143925 <L00143925@student.lyit.ie>
2021-03-31 00:20:46 +0100

    mainMethod committed2

--- src/com/Dinoteck/Main_method.java
+++ src/com/Dinoteck/Main_method.java
@@ -10,7 +10,6 @@ public class Main_method {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random(10 +1);
-        int size = r.nextInt()+10;
-        System.out.println(size);
+
     }
 }
