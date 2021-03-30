d1c1ba2e3889954f8bfe950ade48e115f4f65e36
L00143925 <L00143925@student.lyit.ie>
2021-03-30 19:57:57 +0100

    proj commit

---
+++ src/LocaPack/LocalRepo.java
@@ -0,0 +1,24 @@
+package LocaPack;
+
+import java.util.Scanner;
+
+public class LocalRepo {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("please enter your favourite colour");
+        String colour= sc.next();
+        LocalRepo.ShowColour(colour);
+    }
+
+    private static String ShowColour(String colour) {
+        String danger = "be careful";
+        String lucky =" you are a lucky dude";
+        if(colour.equalsIgnoreCase("red")){
+            System.out.println("my favourite colours is: "+ colour+ " and you better  " +danger);
+        }else{
+            System.out.println("my favourite colours is: "+ colour);
+
+        }
+       return colour;
+    }
+}
