21a6699851f37d0a5469cd07121870c1e83bd2cb
L00143925 <L00143925@student.lyit.ie>
2021-03-31 02:12:44 +0100

    saved for future reference2

--- src/com/Dinoteck/FollowUp.java
+++ src/com/Dinoteck/FollowUp.java
@@ -1,19 +1,20 @@
 package com.Dinoteck;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FollowUp {
     public static void main(String[] args) {
-        Family FF = new Family("udoku");
+       Family FF = new Family("Kelleb");
+          FF.add(new Family("Ezeoha"));
         System.out.println(FF.getName());
-
     }
 
 }
  class Family{
     private String name;
-    List<Family> family;
+   ArrayList<Family> family;
 
     public String getName() {
         return name;
@@ -36,7 +37,9 @@ public class FollowUp {
     }
 
 
-
+     public void add(Family families){
+                    families = new Family(name);
+     }
 
 
 }
