f304f36bbda1385bd0fa87ebc7a43f49e6171cb2
L00143925 <L00143925@student.lyit.ie>
2021-04-16 16:58:35 +0100

    updated code

--- src/com/Dinoteck/FollowUp.java
+++ src/com/Dinoteck/FollowUp.java
@@ -6,40 +6,11 @@ import java.util.List;
 
 public class FollowUp {
     public static void main(String[] args) {
-       Family FF = new Family("Kelleb");
-          FF.add(new Family("Ezeoha"));
-        System.out.println(FF.getName());
-    }
-
+ boolean eat = true;
+if(eat){
+System.out.println("Enjoy the food");
+}else{
+System.out.println("Stop being lazy and find something doing");
 }
- class Family{
-    private String name;
-   ArrayList<Family> family;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "My Family is : " + name + '\'' +
-                ", family=" + family +
-                '}';
-    }
-
-    Family(String  name){
-        this.name= name;
-        family = new ArrayList<>();
-    }
-
-
-     public void add(Family families){
-                    families = new Family(name);
-     }
-
-
+      
 }
